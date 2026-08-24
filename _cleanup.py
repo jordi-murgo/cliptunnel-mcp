143fec9923b337fa4d6cc194fcc694bde270cbea
Jordi Murgó <jordi.murgo@gmail.com>
2026-08-24 17:52:04 +0200

    chore: remove temp cleanup script

--- _cleanup.py
+++
@@ -1,6 +0,0 @@
-import subprocess, os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-os.remove("_commit.py")
-subprocess.run(["git", "add", "-A"])
-subprocess.run(["git", "commit", "-m", "chore: remove temp commit script"])
-subprocess.run(["git", "push"])
